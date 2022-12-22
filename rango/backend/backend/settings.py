44f78a073ff958e67d454fa932ceaf589256503c
huncholane <huncho@ghostsystems.io>
2022-12-22 08:51:44 -0800

    dev forces you to have venv

--- rango/backend/backend/settings.py
+++ rango/backend/backend/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [f'.{os.environ.get("DOMAIN")}', f'.{os.environ.get("NGINX_HOST")}', '.localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = []
+for host in ALLOWED_HOSTS:
+    CSRF_TRUSTED_ORIGINS += [f'https://*{host}', f'http://*{host}']
 
 
 # Application definition
